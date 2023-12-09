5a85d2eab2dd9f6e7f41de2d06aedf395cb9e8a7
Alex5625 <ahernandez23@alumnos.utalca.cl>
2023-12-09 10:53:43 -0300

    act

--- utiles.rs
+++ utiles.rs
@@ -1,20 +1,4 @@
 
-pub fn texto_numero(campo:String) -> i32 {
-    loop {
-        println!("Ingrese un número para el/la {}: ",campo);
-        let mut numero = String::new();
-        let stdin = std::io::stdin();
-        stdin.read_line(&mut numero).unwrap();
-        let numero: i32 = match numero.trim().parse(){
-            Ok(numero) => numero,
-            Err(_) => {
-                println!("Error, no es un número");
-                continue;
-            },
-        };
-        return numero;
-    }
-}
 
 
 pub fn ingreso_texto(campo: String) -> String {
